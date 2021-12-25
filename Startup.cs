ea19f36613dff58182ff01f01b173f67f54b530d
jsakamoto <junichi.jp@gmail.com>
2021-12-25 21:37:55 +0900

    Share a single communicator object among all clients

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,8 @@ namespace raspai_blazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddSingleton<SerialCommunicator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
